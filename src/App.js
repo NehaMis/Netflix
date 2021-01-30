ae21cde49327cfeef4872f7b61a184bea05354c5
Neha Mishra <nehamishra.developer@gmail.com>
2021-01-31 01:47:17 +0530

    jumbotron, routing, opt form, fag, footer Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from "./pages";
+
+export default function App() {
+    return (
+        <>
+            <Router>
+                <Route exact path={ROUTES.HOME}>
+                    <Home />
+                </Route>
+                <Route exact path={ROUTES.SIGN_IN}>
+                    <Signin />
+                </Route>
+                <Route exact path={ROUTES.SIGN_UP}>
+                    <Signup />
+                </Route>
+                <Route exact path={ROUTES.BRWOSE}>
+                    <Browse />
+                </Route>
+            </Router>
+        </>
+    );
 }
-
-export default App;
